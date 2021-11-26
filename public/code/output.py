d272bc601a2d42a865eb4177be0667c89f60ba96
bailey2018 <bailey2018@my.fit.edu>
2021-11-26 07:02:19 -0500

    Style Changes

--- public/code/output.py
+++ public/code/output.py
@@ -1,11 +1,7 @@
 X = None
 
 
-X = 10
+X = 15
 
-if X == 2:
-  print('Good Morning, World!')
-elif X > 5:
-  print('Good Night, World.')
-else:
-  print('Good Bye, World.')
+if X <= 14:
+  print('You did it! Congratulations!')
