32edf147978b51126d80255686195ada5b2cd2a7
bailey2018 <bailey2018@my.fit.edu>
2022-02-06 20:28:04 -0500

    Tutorials Final Revision For Milestone

--- public/code/output.py
+++ public/code/output.py
@@ -1,24 +1,11 @@
-from PyBlockFunctions import *
-from tutorials.looping.looping_tutorial_page_1_gwd import *
+X = None
 
-move_left()
-move_down()
-move_down()
-rotate_left()
-if False:
-  pass
-move_down()
-if False:
-  pass
-move_right()
-move_right()
-move_right()
-move_right()
-rotate_left()
-if False:
-  pass
-move_right()
-move_up()
-move_up()
-if False:
-  pass
+
+X = 10
+
+if X != 2:
+  print('Good Morning, World!')
+elif X == 2:
+  print('Good Night, World.')
+else:
+  print('Goodbye, World...')
