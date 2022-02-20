91c81abb638ac7d49a658ba9af7abd3c3a3d67fa
bailey2018 <bailey2018@my.fit.edu>
2022-02-20 11:11:09 -0500

    looping tutorial setup

--- public/scripts/tutorials/looping/looping_tutorial_page_1_scripts.js
+++ public/scripts/tutorials/looping/looping_tutorial_page_1_scripts.js
@@ -1,25 +1,21 @@
 $(document).ready(function () {
-    // Listen for when the submit button for Try It one is clicked.
-    $("#submitButtonOne").click(function () {
-        // Get the info the user has submitted.
-        let ans1 = $("#inputOne").val();
-        let ans2 = $("#inputTwo").val();
-
-        // Check to see if the answer is right.
-        if (ans1 == "Head" || ans1 == "head") {
-			if (ans2 == "Body" || ans2 == "body"){
-				$("#resultOne").text("Correct!");
-				$("#resultOne").css("background-color", "LimeGreen");
-				
-			}
-			else {
-				$("#resultOne").text("Incorrect, try again.");
-				$("#resultOne").css("background-color", "Crimson");
-			}
-        }
-        else {
-            $("#resultOne").text("Incorrect, try again.");
-            $("#resultOne").css("background-color", "Crimson");
-        }
+    // Listen for when the correct button is clicked.
+    $("#multiChoiceB").click(function () {
+		$("#resultOne").text("Correct!");
+		$("#resultOne").css("background-color", "LimeGreen");
+    });
+	
+    // Listen for when the other buttons are clicked.
+    $("#multiChoiceA").click(function () {
+		$("#resultOne").text("Incorrect, try again.");
+		$("#resultOne").css("background-color", "Crimson");
+    });
+    $("#multiChoiceC").click(function () {
+		$("#resultOne").text("Incorrect, try again.");
+		$("#resultOne").css("background-color", "Crimson");
+    });
+    $("#multiChoiceD").click(function () {
+		$("#resultOne").text("Incorrect, try again.");
+		$("#resultOne").css("background-color", "Crimson");
     });
 });
--- public/scripts/tutorials/looping/looping_tutorial_page_5_scripts.js
+++ public/scripts/tutorials/looping/looping_tutorial_page_5_scripts.js
@@ -1,81 +1,17 @@
 $(document).ready(function () {
-	
-	// make qS a shortcut for document.querySelector for usage in answer checking.
-	const qS = document.querySelector.bind(document);
-	
-    // Listen for when the submit button for the Try It is clicked.
+    // Listen for when the submit button for Try It one is clicked.
     $("#submitButtonOne").click(function () {
+        // Get the info the user has submitted.
+        let ans1 = $("#inputOne").val();
 
-		// get program from workspace.
-		let code = Blockly.Python.workspaceToCode(Blockly.getMainWorkspace());
-
-		// set the filename.
-		const fileName = "output.py";
-		
-		// listen for the output of the code execution.
-		var socket = io();
-		
-		// save program first before running.
-		//console.log(code);
-		socket.emit('save', fileName, code, (response) => {
-			console.log(response.status);
-		});
-
-		// now we can run program.
-		socket.emit('run', fileName, (response) => {
-			console.log(response.status);
-		});
-		
-		socket.on('progOut', function (data) {
-			// jQuery method of inputting data into an HTML element.
-			let ans = data.output;
-			$("#resultOne").text(ans);
-			// Check to see if the answer is right.
-			if (ans == "XXXXXXXXXX\r\nXXXXXXXXXX\r\nXXXXXXXXXX\r\nXXXXXXXXXX\r\nXXXXXXXXXX\r\n") {
-				$("#resultOne").text("Nice job!");
-				$("#resultOne").css("background-color", "LimeGreen");
-				$("#userOut").text("");
-
-				// Replace the submit button with a golden star?
-			}
-			else {
-				$("#resultOne").text("Incorrect, try again.");
-				$("#resultOne").css("background-color", "Crimson");
-				$("#userOut").text(ans);
-			}
-			
-			
-			});
-
+        // Check to see if the answer is right.
+        if (ans1 == "Interesting" || ans1 == "interesting"|| ans1 == "Complicated"|| ans1 == "complicated") {
+			$("#resultOne").text("Correct!");
+			$("#resultOne").css("background-color", "LimeGreen");
+        }
+        else {
+            $("#resultOne").text("Incorrect, try again.");
+            $("#resultOne").css("background-color", "Crimson");
+        }
     });
-	
-	//Tutorial: Looping
-    var workspaceTut = Blockly.inject('blocklyDiv', {
-        toolbox: document.getElementById('toolbox'),
-        scrollbars: false,
-    });
-
-    /*var xmlContentTut = '<xml id="initiated" style="display: none">' +
-		'	<block type="variables_set" deletable="false" movable="false">' +
-		'		<field name="VAR">X</field>' +
-		'		<value name="VALUE">' +
-		'			<block type="math_number" deletable="false" movable="false">' +
-		'				<field name="NUM">0</field>' +
-		'			</block>' +
-		'		</value>' +
-		'	</block>' +
-        ' 	<block type="controls_whileUntil" inline="false" deletable="false" x="0" y="50">' +
-        ' 	</block>' +
-        ' 	<block type="text_print" inline="false" deletable="false" x="0" y="150">' +
-        ' 		<value name="TEXT">' +
-        ' 			<block type="variables_get" deletable="false">' +
-		'				<field name="VAR">X</field>' +
-        ' 			</block>' +
-        ' 		</value>' +
-        ' 	</block>' +
-        '</xml>';*/
-	var xmlContentTut = '<xml id="initiated" style="display: none">'+
-        '</xml>';
-    domTBGK = Blockly.Xml.textToDom(xmlContentTut);
-    Blockly.Xml.domToWorkspace(domTBGK, workspaceTut);
 });
